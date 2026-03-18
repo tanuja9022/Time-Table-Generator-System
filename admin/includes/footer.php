1ddbbaa36763a3fa1fc3309314cc882974da702a
tanuja9022 <tanujafarne@gmail.com>
2026-03-18 12:50:57 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,7 @@
+ <div class="row">
+                        <div class="col-lg-12">
+                            <div class="footer">
+                                <p>Time Table Generator</p>
+                            </div>
+                        </div>
+                    </div>
---
+++ admin/includes/sidebar.php
@@ -0,0 +1,30 @@
+<div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
+        <div class="nano">
+            <div class="nano-content">
+                <ul>
+
+                    <li class="label">Main</li>
+                    <li><a href="dashboard.php" ><i class="ti-home"></i>Dashboard</a></li>
+                   <li><a href="class.php"><i class="ti-files"></i>Class</a>
+                    </li>
+                     <li><a href="subject.php"><i class="ti-files"></i>Subject</a>
+                    </li>
+                    
+                    <li><a class="sidebar-sub-toggle"><i class="ti-user"></i>  Teacher  <span class="sidebar-collapse-icon ti-angle-down"></span></a>
+                        <ul>
+                             <li><a href="add-teacher.php">Add Teacher</a>
+                    </li>
+                            <li><a href="manage-teacher.php">Manage Teacher</a></li>
+                           
+                        </ul>
+                    </li>
+                      <li><a href="subjectclass-allocation.php"><i class="ti-files"></i>Subject Allocation</a>
+                    </li>
+                   
+                  <li><a href="class-rooms.php" ><i class="ti-file"></i>Class Rooms</a></li> 
+                  <li><a href="time-slots.php" ><i class="ti-file"></i>Time Slots</a></li> 
+                   <li><a href="time-table.php" style="color:red;"><strong><i class="ti-file"></i>Time Table</a></strong></li> 
+                </ul>
+            </div>
+        </div>
+    </div>      
